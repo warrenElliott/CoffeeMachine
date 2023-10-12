353d8e293a557011309c3bd8a9e375e6a4396ad4
warrenElliott <w.elliott511@gmail.com>
2023-10-12 06:03:22 +0100

    test

---
+++ CoffeeData.py
@@ -0,0 +1,36 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+quarter = 0.25
+penny = 0.01
+nickle = 0.05
+dime = 0.10
--- main.py
+++ main.py
@@ -1,46 +1,14 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+import CoffeeData
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-
-quarter = 0.25
-penny = 0.01
-nickle = 0.05
-dime = 0.10
-
+c = CoffeeData
 
 def espresso_maker():
     print("espresso made")
 
+
 def reporter():
-    print("Water:", resources["water"], "\nMilk: ", resources["milk"], "\nCoffee:", resources["coffee"])
+    print("Water:", c.resources["water"], "\nMilk: ", c.resources["milk"], "\nCoffee:", c.resources["coffee"])
+
 
 def selector():
     reporter()
@@ -56,7 +24,7 @@ def selector():
     money_entered = quarter_counter * 0.25 + dime_counter * 0.10 + nickle_counter * 0.05 + penny_counter * 0.01
 
     if selection == "espresso":
-        esp = MENU["espresso"]["cost"]
+        esp = c.MENU["espresso"]["cost"]
         if money_entered > esp:
             change = money_entered - esp
             print("your change is: ", change)
